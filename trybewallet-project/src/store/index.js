a21c4b3cce49ec94a0451c85e5ca497eedbb9853
Heitor da Costa <heitor2000costa@gmail.com>
2022-09-07 20:32:32 -0300

    excluindo composewitdevtools

--- trybewallet-project/src/store/index.js
+++ trybewallet-project/src/store/index.js
@@ -1,8 +1,7 @@
-import { composeWithDevTools } from "@redux-devtools/extension";
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index.js';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
